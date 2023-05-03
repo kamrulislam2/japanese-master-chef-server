fe885609aef81313481638b19467a038bc279958
kamrulislam2 <kamrulislamABK@gmail.com>
2023-05-03 13:01:56 +0600

    Image link updated

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allChefData = require("./Data/japaneseChef.json");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.json({ siteName: "Chef recipe hunter" });
+});
+
+app.get("/allChefData", (req, res) => {
+  res.send(allChefData);
 });
 
 app.listen(port, () => {

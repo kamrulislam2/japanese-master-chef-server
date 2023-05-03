10553111ba096f5423e31b5173af9f1f9326d752
kamrulislam2 <kamrulislamABK@gmail.com>
2023-05-03 20:02:36 +0600

    Single data by Id sended

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get("/allChefData", (req, res) => {
   res.send(allChefData);
 });
 
+app.get("/allChefData/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChefData = allChefData.chefs.find((chef) => chef.chefId === id);
+  res.send(singleChefData);
+});
+
 app.listen(port, () => {
   console.log("Chef recipe hunter server is running");
 });
